fb65e81a69bed8adfb658c294464329bc9c63a9a
narasingha1 <narasinghahembram0@gmail.com>
2026-06-18 12:49:54 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello phone se Java chal gaya!");
-        System.out.println("2 + 3 = " + (2+3));
-    }
-}
